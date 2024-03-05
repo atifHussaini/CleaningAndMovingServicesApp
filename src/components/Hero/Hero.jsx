aa2c80eb6bc588f3417c357aa73f93eaa82c97a0
Atif Hussaini <114316821+AtifHussaini@users.noreply.github.com>
2024-03-05 15:26:11 -0600

    finished static part

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,6 +2,11 @@ import React from "react";
 import "./Hero.css";
 import Header from "../Header/Header";
 
+import hero_image from "../../assets/hero_image.png";
+import hero_image_back from "../../assets/hero_image_back.png";
+import Heart from "../../assets/heart.png";
+import Calories from "../../assets/calories.png";
+
 const Hero = () => {
   return (
     <div className="hero">
@@ -44,8 +49,35 @@ const Hero = () => {
             <span>Customers</span>
           </div>
         </div>
+
+        {/* Hero Buttons */}
+        <div className="hero-buttons">
+          <button className="btn">Get Started</button>
+          <button className="btn">Learn More</button>
+        </div>
       </div>
-      <div className="right-h">Right side</div>
+      <div className="right-h">
+        <button className="btn">Join Now</button>
+
+        <div className="heart-rate">
+          <img src={Heart} alt="" />
+          <span>Heart Rate</span>
+          <span>116 BPM</span>
+        </div>
+
+        {/* Hero Images */}
+        <img src={hero_image} alt="" className="hero-image" />
+        <img src={hero_image_back} alt="" className="hero-image-back" />
+
+        {/* Calories*/}
+        <div className="calories">
+          <img src={Calories} alt=""></img>
+          <div>
+            <span>Calories Burned</span>
+            <span>220 kcal</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
